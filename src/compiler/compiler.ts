12a54ebde9e9b96aac0dcc3e2e1365cccc89a1f5
sinclairzx81 <haydn.developer@gmail.com>
2026-07-12 06:04:31 +0900

    Revision 0.31.30

--- src/compiler/compiler.ts
+++ src/compiler/compiler.ts
@@ -87,42 +87,6 @@ namespace Character {
     return code >= 48 && code <= 57
   }
 }
-// ------------------------------------------------------------------
-// StringUtil
-// ------------------------------------------------------------------
-export namespace StringUtil {
-  /**
-   * Unquoted string values are embedded into single-quote string. This
-   * function ensures that embedded string cannot be escaped.
-   */
-  export function EscapeSingleQuote(value: string): string {
-    return JSON.stringify(value).slice(1, -1).replace(/'/g, "\\'")
-  }
-}
-// -------------------------------------------------------------------
-// MemberExpression
-// -------------------------------------------------------------------
-namespace MemberExpression {
-  function IsFirstCharacterNumeric(value: string) {
-    if (value.length === 0) return false
-    return Character.IsNumeric(value.charCodeAt(0))
-  }
-  function IsAccessor(value: string) {
-    if (IsFirstCharacterNumeric(value)) return false
-    for (let i = 0; i < value.length; i++) {
-      const code = value.charCodeAt(i)
-      const check = Character.IsAlpha(code) || Character.IsNumeric(code) || Character.DollarSign(code) || Character.IsUnderscore(code)
-      if (!check) return false
-    }
-    return true
-  }
-  function EscapeHyphen(key: string) {
-    return StringUtil.EscapeSingleQuote(key)
-  }
-  export function Encode(object: string, key: string) {
-    return IsAccessor(key) ? `${object}.${key}` : `${object}['${EscapeHyphen(key)}']`
-  }
-}
 // -------------------------------------------------------------------
 // Identifier
 // -------------------------------------------------------------------
@@ -140,13 +104,20 @@ namespace Identifier {
     return buffer.join('').replace(/__/g, '_')
   }
 }
-// -------------------------------------------------------------------
-// LiteralString
-// -------------------------------------------------------------------
-namespace LiteralString {
-  export function Escape(content: string) {
-    return StringUtil.EscapeSingleQuote(content)
-  }
+// ------------------------------------------------------------------
+// StringConstant
+// ------------------------------------------------------------------
+function StringConstant(value: string): string {
+  if (!IsString(value)) throw Error('ConstantString: Not a String')
+  const canonical = JSON.stringify(value).slice(1, -1)
+  const escaped = canonical.replace(/'/g, "\\'")
+  return `'${escaped}'`
+}
+// ------------------------------------------------------------------
+// MemberExpression
+// ------------------------------------------------------------------
+function MemberExpression(value: string, key: string): string {
+  return `${value}[${StringConstant(key)}]`
 }
 // -------------------------------------------------------------------
 // Errors
@@ -166,7 +137,7 @@ export class TypeCompilerTypeGuardError extends Types.TypeBoxError {
 // -------------------------------------------------------------------
 export namespace Policy {
   export function IsExactOptionalProperty(value: string, key: string, expression: string) {
-    return TypeSystemPolicy.ExactOptionalPropertyTypes ? `('${key}' in ${value} ? ${expression} : true)` : `(${MemberExpression.Encode(value, key)} !== undefined ? ${expression} : true)`
+    return TypeSystemPolicy.ExactOptionalPropertyTypes ? `(${StringConstant(key)} in ${value} ? ${expression} : true)` : `(${MemberExpression(value, key)} !== undefined ? ${expression} : true)`
   }
   export function IsObjectLike(value: string): string {
     return !TypeSystemPolicy.AllowArrayObject ? `(typeof ${value} === 'object' && ${value} !== null && !Array.isArray(${value}))` : `(typeof ${value} === 'object' && ${value} !== null)`
@@ -282,8 +253,10 @@ export namespace TypeCompiler {
   function* TLiteral(schema: Types.TLiteral, references: Types.TSchema[], value: string): IterableIterator<string> {
     if (typeof schema.const === 'number' || typeof schema.const === 'boolean') {
       yield `(${value} === ${schema.const})`
+    } else if (typeof schema.const === 'string') {
+      yield `(${value} === ${StringConstant(schema.const)})`
     } else {
-      yield `(${value} === '${LiteralString.Escape(schema.const)}')`
+      throw Error('Invalid Literal Value')
     }
   }
   function* TNever(schema: Types.TNever, references: Types.TSchema[], value: string): IterableIterator<string> {
@@ -310,11 +283,11 @@ export namespace TypeCompiler {
     if (IsNumber(schema.maxProperties)) yield `Object.getOwnPropertyNames(${value}).length <= ${schema.maxProperties}`
     const knownKeys = Object.getOwnPropertyNames(schema.properties)
     for (const knownKey of knownKeys) {
-      const memberExpression = MemberExpression.Encode(value, knownKey)
+      const memberExpression = MemberExpression(value, knownKey)
       const property = schema.properties[knownKey]
       if (schema.required && schema.required.includes(knownKey)) {
         yield* Visit(property, references, memberExpression)
-        if (Types.ExtendsUndefined.Check(property) || IsAnyOrUnknown(property)) yield `('${knownKey}' in ${value})`
+        if (Types.ExtendsUndefined.Check(property) || IsAnyOrUnknown(property)) yield `(${StringConstant(knownKey)} in ${value})`
       } else {
         const expression = CreateExpression(property, references, memberExpression)
         yield Policy.IsExactOptionalProperty(value, knownKey, expression)
@@ -324,13 +297,13 @@ export namespace TypeCompiler {
       if (schema.required && schema.required.length === knownKeys.length) {
         yield `Object.getOwnPropertyNames(${value}).length === ${knownKeys.length}`
       } else {
-        const keys = `[${knownKeys.map((key) => `'${key}'`).join(', ')}]`
+        const keys = `[${knownKeys.map((key) => `${StringConstant(key)}`).join(', ')}]`
         yield `Object.getOwnPropertyNames(${value}).every(key => ${keys}.includes(key))`
       }
     }
     if (typeof schema.additionalProperties === 'object') {
       const expression = CreateExpression(schema.additionalProperties, references, `${value}[key]`)
-      const keys = `[${knownKeys.map((key) => `'${key}'`).join(', ')}]`
+      const keys = `[${knownKeys.map((key) => `${StringConstant(key)}`).join(', ')}]`
       yield `(Object.getOwnPropertyNames(${value}).every(key => ${keys}.includes(key) || ${expression}))`
     }
   }
@@ -364,7 +337,7 @@ export namespace TypeCompiler {
       yield `${variable}.test(${value})`
     }
     if (schema.format !== undefined) {
-      yield `format('${schema.format}', ${value})`
+      yield `format(${StringConstant(schema.format)}, ${value})`
     }
   }
   function* TSymbol(schema: Types.TSymbol, references: Types.TSchema[], value: string): IterableIterator<string> {
@@ -409,7 +382,7 @@ export namespace TypeCompiler {
   function* TKind(schema: Types.TSchema, references: Types.TSchema[], value: string): IterableIterator<string> {
     const instance = state.instances.size
     state.instances.set(instance, schema)
-    yield `kind('${schema[Types.Kind]}', ${instance}, ${value})`
+    yield `kind(${StringConstant(schema[Types.Kind])}, ${instance}, ${value})`
   }
   function* Visit<T extends Types.TSchema>(schema: T, references: Types.TSchema[], value: string, useHoisting: boolean = true): IterableIterator<string> {
     const references_ = IsString(schema.$id) ? [...references, schema] : references
