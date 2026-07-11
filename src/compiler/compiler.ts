44544668492ace7261c6ef3cf75d7b6feb6502ad
sinclair <haydn.developer@gmail.com>
2026-07-11 15:28:13 +0900

    Ensure Escape for Dynamic String

--- src/compiler/compiler.ts
+++ src/compiler/compiler.ts
@@ -133,6 +133,18 @@ namespace Character {
   }
 }
 // ------------------------------------------------------------------
+// StringUtil
+// ------------------------------------------------------------------
+export namespace StringUtil {
+  /**
+   * Unquoted string values are embedded into single-quote string. This
+   * function ensures that embedded string cannot be escaped.
+   */
+  export function EscapeSingleQuote(value: string): string {
+    return JSON.stringify(value).slice(1, -1).replace(/'/g, "\\'")
+  }
+}
+// ------------------------------------------------------------------
 // MemberExpression
 // ------------------------------------------------------------------
 namespace MemberExpression {
@@ -150,7 +162,7 @@ namespace MemberExpression {
     return true
   }
   function EscapeHyphen(key: string) {
-    return key.replace(/'/g, "\\'")
+    return StringUtil.EscapeSingleQuote(key)
   }
   export function Encode(object: string, key: string) {
     return IsAccessor(key) ? `${object}.${key}` : `${object}['${EscapeHyphen(key)}']`
@@ -178,7 +190,7 @@ namespace Identifier {
 // ------------------------------------------------------------------
 namespace LiteralString {
   export function Escape(content: string) {
-    return content.replace(/'/g, "\\'")
+    return StringUtil.EscapeSingleQuote(content)
   }
 }
 // ------------------------------------------------------------------
